e4eaa0293403f7d5bfeeb0f59d08926a0d0bd9c9
flyher <flyher@users.noreply.github.com>
2016-12-08 21:48:50 +0800

    add 4.import data

---
+++ 4.import data/Model/Movies.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Movies
+    {
+      public Int64 Id { get; set; }
+      public string Name { get; set; }
+      public string Logo { get; set; }
+      public Int64 UpdateId { get; set; }
+
+      public DateTime UpdateDate { get; set; }
+    }
+}
